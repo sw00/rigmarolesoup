cb3c86183cc61a2356eb9cec0bbe5513d6785cd3
Sett Wai <sett.wai@gmail.com>
2012-10-10 09:46:37 +0200

    adding blog index view with built-in admin controls

--- blog.py
+++ blog.py
@@ -1,15 +1,19 @@
 import web
 from google.appengine.ext import db
 from models import Post
+from google.appengine.api import users
 
 urls = (
 		#'', 'reblog',
 		'/?$', 'index',
+		'/create/?$', 'create',
 		'/p/([^/]*)/?$', 'post'
 		)
 
+globals = { 'users' : users }
+
 shared = web.template.render('templates/shared')
-render = web.template.render('templates/blog')
+render = web.template.render('templates/blog', globals=globals)
 
 class reblog:
     def GET(self):
@@ -20,11 +24,17 @@ class index:
 		#get latest 3 blog posts
 		q = Post().all()
 		posts = q.run(limit=3)
+		
+		return shared.layout(render.index(posts, ))
 
-		return shared.layout(render.index(posts))
+class create:
+	def GET(self):
+		if users.is_current_user_admin():
+			return shared.layout(render.create(self))
+		else:
+			raise web.Forbidden()
 
-class post:
-    def GET(self, alias):
-        return alias
+	def POST(self):
+		pass
 
 app_blog = web.application(urls, locals())
--- templates/blog/__init__.py
+++ templates/blog/__init__.py
@@ -6,21 +6,84 @@ def index (posts):
     __lineoffset__ = -4
     loop = ForLoop()
     self = TemplateResult(); extend_ = self.extend
-    extend_([u'<div class="row">\n'])
-    extend_([u'        <div class="span8">\n'])
+    
+    admin = users.is_current_user_admin()
+    
+    if admin:
+        extend_([u'    <h1>', escape_(admin, True), u'</h1>\n'])
+        extend_([u'    <div class="row">\n'])
+        extend_([u'            <div class="span4 offset4 well well-small"><a class="btn btn-small btn-action" href="blog/create">create</a>\n'])
+        extend_([u'                    <button class="btn btn-small btn-primary pull-right">log lout</button>  \n'])
+        extend_([u'            </div>\n'])
+        extend_([u'    </div>\n'])
+        extend_([u'\n'])
+    extend_([u'<div class="row row-fluid" id="content">\n'])
+    extend_([u'        <div class="span8" id="content-blogs">\n'])
     for p in loop.setup(posts):
-        extend_(['        ', u'    <div class="well">\n'])
-        extend_(['        ', u'            <h3>', escape_((p.title), True), u'</h3>\n'])
-        extend_(['        ', u'            <small>', escape_((p.created), True), u'</small>\n'])
-        extend_(['        ', u'            <section class="body">\n'])
-        extend_(['        ', u'                    ', escape_(p.body, False), u'\n'])
-        extend_(['        ', u'            </section>\n'])
+        extend_(['        ', u'    <article>\n'])
+        extend_(['        ', u'    <header style="text-align:center">\n'])
+        extend_(['        ', u'    <h3><a href="p/', escape_(p.title, True), u'" title="', escape_(p.title, False), u'">', escape_((p.title), True), u'</a></h3>\n'])
+        extend_(['        ', u'    <small class="pull-right">', escape_((p.created), True), u'</small>\n'])
+        extend_(['        ', u'    <div class="btn-group">\n'])
+        extend_(['        ', u'            <a href="#edit" class="btn btn-mini">edit</a>\n'])
+        extend_(['        ', u'            <a href="#delete" class="btn btn-mini">delete</a>\n'])
         extend_(['        ', u'    </div>\n'])
-    extend_([u'        </div>\n'])
+        extend_(['        ', u'    </header>\n'])
+        extend_(['        ', u'    <div class="body">\n'])
+        extend_(['        ', u'                    ', escape_(p.body, False), u'\n'])
+        extend_(['        ', u'    </div>\n'])
+    extend_([u'        </article>\n'])
     extend_([u'</div>\n'])
+    extend_([u'<div class="span2 pull-left" id="aside">\n'])
+    extend_([u'        <form id="search" action="search" method="POST">\n'])
+    extend_([u'                <input type="search" placeholder="search..."></input>\n'])
+    extend_([u'        </form>\n'])
+    extend_([u'        <hr/>\n'])
+    extend_([u'        <h4>Categories</h4>\n'])
+    extend_([u'        <ul>\n'])
+    extend_([u'                <li>stuff</li>\n'])
+    extend_([u'        </ul>\n'])
+    extend_([u'</div>\n'])
+    extend_([u'</div>\n'])
+    extend_([u'\n'])
+    extend_([u'<script type="text/javascript" language="javascript">\n'])
+    extend_([u'        \n'])
+    extend_([u'</script>\n'])
 
     return self
 
 index = CompiledTemplate(index, 'templates/blog/index.html')
 join_ = index._join; escape_ = index._escape
+
+# coding: utf-8
+def create (self):
+    __lineoffset__ = -4
+    loop = ForLoop()
+    self = TemplateResult(); extend_ = self.extend
+    extend_([u'<form action="" method="POST">\n'])
+    extend_([u'        <legend>New Blog Entry</legend>\n'])
+    extend_([u'        <fieldset>\n'])
+    extend_([u'        <label>Title</label>\n'])
+    extend_([u'        <input type="text"></input>\n'])
+    extend_([u'</fieldset>\n'])
+    extend_([u'<fieldset>\n'])
+    extend_([u'        <label>Body</label>\n'])
+    extend_([u'        <input type="textarea"></input>\n'])
+    extend_([u'</fieldset>\n'])
+    extend_([u'<fieldset>\n'])
+    extend_([u'        <label>References</label>\n'])
+    extend_([u'        <select>\n'])
+    extend_([u'                <option value="">None</option>\n'])
+    extend_([u'        </select>\n'])
+    extend_([u'</fieldset>\n'])
+    extend_([u'<fieldset>\n'])
+    extend_([u'        <label>Tags</label>\n'])
+    extend_([u'        <input type="text"></input>\n'])
+    extend_([u'</fieldset>\n'])
+    extend_([u'<input type="submit">Save</input>\n'])
+
+    return self
+
+create = CompiledTemplate(create, 'templates/blog/create.html')
+join_ = create._join; escape_ = create._escape
 
