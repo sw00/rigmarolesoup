4509c142e7c3ec782f4335f239edfe7b7d499890
Sett Wai <sett.wai@gmail.com>
2012-10-11 15:05:55 +0200

    Revert "refactored models to use NDB"
    This reverts commit ab895e3f3c83f62322e61dca6717a9aeaddefe33.

--- models.py
+++ models.py
@@ -1,21 +1,20 @@
-from google.appengine.ext import ndb
+from google.appengine.ext import db
 
-class Post(ndb.Model):
-	title = ndb.StringProperty()
-	body = ndb.TextProperty()
-	created = ndb.DateTimeProperty(auto_now_add=True)
-	category = ndb.KeyProperty()
-	tags = ndb.KeyProperty(repeated=True)
+class Post(db.Model):
+	title = db.StringProperty()
+	body = db.TextProperty()
+	created = db.DateTimeProperty(auto_now_add=True)
 
-	@classmethod
-	def fetch_all(cls, *filters):
-		return cls.query(*filters).order(cls.created)
+class Resource(db.Model):
+	name = db.StringProperty()
+	mimetype = db.StringProperty()
+	uri = db.StringProperty()
 
-class Category(ndb.Model):
-	name = ndb.StringProperty()
-	desc = ndb.StringProperty(indexed=False)
+class Category(db.Model):
+	name = db.StringProperty()
+	desc = db.StringProperty()
 
+class Tags(db.Model):
+	name = db.StringProperty()
 
-class Tag(ndb.Model):
-	name = ndb.StringProperty()
 
