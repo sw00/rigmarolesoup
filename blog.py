44803f5ca2808c7699943d51f155047e976c523f
Sett Wai <sett.wai@gmail.com>
2012-11-12 16:17:45 +0200

    implemented url handling for date

--- blog.py
+++ blog.py
@@ -2,11 +2,12 @@ import web
 from web.contrib.template import render_mako
 from models import Category, Entry
 from google.appengine.ext import ndb
+from datetime import datetime, timedelta
 
 urls = (
 		'^/?$', 'index',
 		'/([\w\d-]+)/?', 'entry',
-		'/(\d{4})/(\d{2})/([-\w]+)/?', 'fetchByDate'
+		'/(\d{4})/(\d{2})/(\d{2})/([-\w\d]+)/?', 'fetchByDate'
 		)
 
 render = render_mako(
@@ -60,5 +61,21 @@ class entry:
 		return render.entry(**data)
 
 class fetchByDate:
-	def GET(self, year, month, title):
-		return year,month,title
+	def GET(self, year, month, day, title):
+		d = datetime(int(year),int(month),int(day))
+		m = timedelta(days=1)
+
+		q = Entry.query(Entry.published==True)
+		#e_list = q.fetch(3, projection=[Entry.title, Entry.timestamp, Entry.intro])
+		entry = q.filter(Entry.timestamp >= d, Entry.timestamp <= d+m, Entry.alias==title).fetch(1)
+
+		categories = Category.query().order(Category.name).fetch()
+		
+		data = {
+				'entry': entry,
+				'categories': categories,
+				'e_list': []
+				}
+
+		return render.entry(**data)
+
