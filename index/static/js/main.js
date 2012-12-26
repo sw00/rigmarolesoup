c8f2b4bb2d97eeeef9c7cc49dcd966d93b6ffdef
Sett Wai <sett.wai@gmail.com>
2012-12-26 12:40:45 +0200

    linkified tweets and fixed entypo webfont

--- index/static/js/main.js
+++ index/static/js/main.js
@@ -1,20 +1,34 @@
-ActivityApp = (function ($) {
+(function ($,W,D) {
+	String.prototype.linkify=function(){
+		return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&amp;;\?\/.=]+/g,function(m){
+			return m.link(m);
+		});
+	};
+	String.prototype.linkuser=function(){
+		return this.replace(/[@]+[A-Za-z0-9-_]+/g,function(u){
+			return u.link("http://twitter.com/"+u.replace("@",""));
+		});
+	};
+	String.prototype.linktag=function(){
+		return this.replace(/[]+[A-Za-z0-9-_]+/,function(t){
+			return t;
+		});
+	};
+
 	$.getJSON(
 		'http://search.twitter.com/search.json?q=from:settface&callback=?', 
 		{},
 		function(data){
-			window.tweets = data.results;
 			var tweets = data.results;	
-			var max = tweets.length < 3 ? tweets.length : 3;
 
-			for (var i=0; i<max; i++){
+			for (var i=0; i<tweets.length; i++){
 				var tweet = tweets[i];
 				tweet.timestamp = new Date(tweet.created_at);
 				tweet.url = '//twitter.com/' + tweet.from_user + '/status/' + tweet.id_str;
 
 				var $item = $('<li>').html(
-					'<span class="entypo"><a href="'+ tweet.url + '">&#62217;</a></span>' + tweets[i].text);
+					'<span class="entypo"><a href="'+ tweet.url + '">&#62217;</a></span>' + tweets[i].text.linkify().linkuser().linktag().replace(/<a/g,'<a target="_blank"'));
 				$('#activity').append($item);
 			}
-		});
-})(jQuery);
+		}); 
+})(jQuery, window, document);
